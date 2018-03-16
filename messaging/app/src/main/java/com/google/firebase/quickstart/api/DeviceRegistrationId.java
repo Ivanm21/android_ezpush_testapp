8615cb3f1da07236b2ee1c3d331f69e30d7ef399
ivanm <ivanmalyshenko@gmail.com>
2018-03-16 11:47:10 +0200

    First commit

---
+++ messaging/app/src/main/java/com/google/firebase/quickstart/api/DeviceRegistrationId.java
@@ -0,0 +1,8 @@
+package com.google.firebase.quickstart.api;
+
+/**
+ * Created by ivanm on 12/19/2017.
+ */
+
+public class DeviceRegistrationId {
+}
---
+++ messaging/app/src/main/java/com/google/firebase/quickstart/httpClient/httpClient.java
@@ -0,0 +1,8 @@
+package com.google.firebase.quickstart.httpClient;
+
+/**
+ * Created by ivanm on 12/18/2017.
+ */
+
+public class httpClient {
+}
